6ec7434bbfbcf28cc6fd2e3ad69c99381326ad5e
Butler <butlermuwo@gmail.com>
2023-09-28 02:59:09 +0200

    Add more notes

--- Basics/27-09-2023.js
+++ Basics/27-09-2023.js
@@ -22,3 +22,17 @@ let actual = max - 13;
 
 let percentage = actual / max;
 
+
+//Unary && Binary Operators
+
+let mangoes = "10";
+let apples = "12";
+
+alert( +mangoes + +apples ); //prepending the Unary Operator to each variable name converts the string to a number.
+
+alert( mangoes + apples); // Binary operators concatenate strings so the result = mangoesapples.
+
+//Increment Prefix && Postfix
+let counter = 0;
+counter++ // This outputs the previous value so it's = 0; postfix
+++counter // This outputs a new number 1; prefix
