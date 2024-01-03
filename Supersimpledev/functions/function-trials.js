19bd25d2656fbdb86d8df21e876e4bae1a1ba60f
Butler <butlermuwo@gmail.com>
2024-01-04 01:40:26 +0200

    add temperature converter

--- Supersimpledev/functions/function-trials.js
+++ Supersimpledev/functions/function-trials.js
@@ -9,3 +9,43 @@ function greet(name) {
 greet('Butler Shimaluwani')
 greet('Zephyr')
 greet()
+
+// Temp converter
+
+function convertToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32;
+    console.log(fahrenheit)
+}
+
+convertToFahrenheit(25);
+
+function convertToCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    console.log(celsius)
+}
+
+convertToCelsius(86);
+
+
+
+
+function convertTemperature(degrees, unit) {
+
+    function convertToCelsius(fahrenheit) {
+        return (fahrenheit - 32) * 5/9;     
+    }
+
+    function convertToFahrenheit(celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    if (unit.toLowerCase() === 'c') {
+        console.log(`${degrees} Celsius is ${convertToFahrenheit(degrees)} Fahrenheit`);
+    } else if (unit.toLowerCase() === 'f') {
+        console.log(`${degrees} Fahrenheit is ${convertToCelsius(degrees)} Celsius`);
+    } else {
+        console.log('Invalid input');
+    }
+}
+
+convertTemperature(86, 'f');
