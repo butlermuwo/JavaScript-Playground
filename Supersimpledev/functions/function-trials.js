4f3bdb682fa8feb8169042ca273af641d776f571
Butler <butlermuwo@gmail.com>
2024-01-04 00:57:26 +0200

    Add first example of functions in action

---
+++ Supersimpledev/functions/function-trials.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    if (!name) {
+        console.log('Hey there!')
+    } else {
+        console.log(`Hello ${name}!`)
+    }
+}
+
+greet('Butler Shimaluwani')
+greet('Zephyr')
+greet()
